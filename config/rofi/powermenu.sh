04b4dbc498674b1ceae7fcdc73704c8fe44d367e
IamJomm <artemlantsman05@gmail.com>
2023-07-31 00:05:22 +0300

    Update

--- config/rofi/powermenu.sh
+++ config/rofi/powermenu.sh
@@ -1,11 +1,24 @@
-#! /bin/sh
+#! /bin/bash
 
-chosen=$(printf "Power Off\nSuspend\nHibernate\nRestart\nLock" | rofi -dmenu)
+chosen=$(printf "Power Off\nSuspend\nHibernate\nRestart\nLog Out" | rofi -dmenu -p "Power menu:")
 
 case "$chosen" in
-    "Power Off") poweroff ;;
+    "Power Off")
+        chosen=$(printf "Yes\nNo" | rofi -dmenu -selected-row 1 -p "Are you sure?")
+        if [[ "$chosen" == "Yes" ]]; then
+            poweroff
+        fi ;;
     "Suspend") systemctl suspend ;;
     "Hibernate") systemctl hibernate ;;
-    "Restart") reboot ;;
+    "Restart")
+        chosen=$(printf "Yes\nNo" | rofi -dmenu -p "Are you sure?")
+        if [[ "$chosen" == "Yes" ]]; then
+            reboot
+        fi ;;
+    "Log Out")
+        chosen=$(printf "Yes\nNo" | rofi -dmenu -p "Are you sure?")
+        if [[ "$chosen" == "Yes" ]]; then
+            hyprctl dispatch exit
+        fi ;;
     *) exit 1 ;;
 esac
