6a00fe65e0e3882447634ac31bc6aed823eddda3
Artem <artemlantsman05@gmail.com>
2023-09-27 22:46:01 +0300

    Update

---
+++ config/swaylock/lock.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+swaylock --clock \
+    --indicator \
+    --indicator-radius 100 \
+    -indicator-thickness 10 \
+    --screenshot \
+    --effect-blur 7x3 \
+    --effect-vignette 0.5:0.5 \
+    --text-color "#e0def4" \
+    --ring-color "#908caa" \
+    --key-hl-color "#ebbcba" \
+    --line-color "#191724" \
+    --inside-color "#00000000" \
+    --separator-color "#00000000" \
+    --ring-wrong-color "#eb6f92" \
+    --inside-wrong-color "#eb6f92" \
+    --ring-clear-color "#9ccfd8" \
+    --inside-clear-color "#9ccfd8"
